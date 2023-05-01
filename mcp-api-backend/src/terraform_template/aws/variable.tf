20112b2028982cab7aac8707f9b1b5c8a688a5ed
parkjiyun <pjyun05200@gmail.com>
2023-05-01 19:08:06 +0900

    aws subnet 관련 변수 설정

--- mcp-api-backend/src/terraform_template/aws/variable.tf
+++ mcp-api-backend/src/terraform_template/aws/variable.tf
@@ -26,3 +26,11 @@ variable "aws_public_key_name" {
     type = string
     description = "SSH 키를 입력해주세요."
 }
+
+
+# subnet
+variable "aws_subnet_name" {
+    type = string
+    description = "SUBNET 이름을 특수문자 없이 입력하세요."
+    default = "aws subnet name"
+}
