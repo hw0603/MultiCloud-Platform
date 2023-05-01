2866d283c90b93bb5e93de9ebd9117eb5a724ece
HyungUk Ryu <hw0603@naver.com>
2023-05-01 20:18:34 +0900

    Fix: /{id} 호출이 /list 를 가리는 문제 수정

--- mcp-api-backend/api/v1/instance.py
+++ mcp-api-backend/api/v1/instance.py
@@ -15,6 +15,18 @@ EC2_Cloudwatch_Metrics = {
 }
 
 
+@router.get("/list")
+def get_instance_list():
+    # TODO: Authentication 추가 후에는 토큰에서 각 프로바이더의 인증 정보를 가져와서 사용할 것
+    AWS_ACCESS_KEY_ID = settings.AWS_ACCESS_KEY_ID
+    AWS_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
+    AWS_DEFAULT_REGION = 'ap-northeast-2'
+
+    instance_list = instance_service.get_instance_list(
+        AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION)
+    return ApiResponse.with_data(ApiStatus.SUCCESS, "success", instance_list)
+
+
 @router.get("/{instance_id}")
 def get_instance_state(instance_id: str, metrics: Optional[List[str]] = Query(None)):
     ''' 사용자로부터 조회할 지표들을 받아서 조회 결과를 반환 '''
@@ -38,12 +50,3 @@ def get_instance_state(instance_id: str, metrics: Optional[List[str]] = Query(No
     return ApiResponse.with_data(ApiStatus.SUCCESS, "success", state)
 
 
-@router.get("/list")
-def get_instance_list():
-    # TODO: Authentication 추가 후에는 토큰에서 각 프로바이더의 인증 정보를 가져와서 사용할 것
-    AWS_ACCESS_KEY_ID = settings.AWS_ACCESS_KEY_ID
-    AWS_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
-    AWS_DEFAULT_REGION = 'ap-northeast-2'
-
-    instance_list = instance_service.get_instance_list(AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_DEFAULT_REGION)
-    return ApiResponse.with_data(ApiStatus.SUCCESS, "success", instance_list)
