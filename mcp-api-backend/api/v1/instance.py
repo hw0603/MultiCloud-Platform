e2849a82aa167f0cfda77aecac8febe60310d69b
HyungUk Ryu <hw0603@naver.com>
2023-05-01 19:29:37 +0900

    Comment: 주석 추가

--- mcp-api-backend/api/v1/instance.py
+++ mcp-api-backend/api/v1/instance.py
@@ -12,7 +12,7 @@ def get_instance_state():
 
 @router.get("/list")
 def get_instance_list():
-    ''' Authentication 추가 후에는 각 프로바이더의 인증 정보를 가져와서 사용할 것 '''
+    ''' Authentication 추가 후에는 토큰에서 각 프로바이더의 인증 정보를 가져와서 사용할 것 '''
     AWS_ACCESS_KEY_ID = settings.AWS_ACCESS_KEY_ID
     AWS_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
     AWS_DEFAULT_REGION = 'ap-northeast-2'
