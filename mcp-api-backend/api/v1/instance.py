87f81f82e52dd428d1ddde5056954957bf056b7a
HyungUk Ryu <hw0603@naver.com>
2023-05-01 20:09:33 +0900

    Feat: AWS Cloudwatch 지표 리스트를 받아 조회하는 기능 추가

--- mcp-api-backend/api/v1/instance.py
+++ mcp-api-backend/api/v1/instance.py
@@ -1,18 +1,33 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Query
 from service import instance_service
+from typing import Optional, List
 from api.api_response import *
 from config.api_config import settings  # for testing
 
 router = APIRouter()
 
-@router.get("/")
-def get_instance_state():
-    state = instance_service.get_instance_state()
-    return state
+@router.get("/{instance_id}")
+def get_instance_state(instance_id: str, metrics: Optional[List[str]] = Query(None)):
+    ''' 사용자로부터 조회할 지표들을 받아서 조회 결과를 반환 '''
+
+    # TODO: Authentication 추가 후에는 토큰에서 각 프로바이더의 인증 정보를 가져와서 사용할 것
+    AWS_ACCESS_KEY_ID = settings.AWS_ACCESS_KEY_ID
+    AWS_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
+    AWS_DEFAULT_REGION = 'ap-northeast-2'
+
+    state = instance_service.get_instance_state(
+        AWS_ACCESS_KEY_ID,
+        AWS_SECRET_ACCESS_KEY,
+        AWS_DEFAULT_REGION,
+        instance_id,
+        metrics
+    )
+    return ApiResponse.with_data(ApiStatus.SUCCESS, "success", state)
+
 
 @router.get("/list")
 def get_instance_list():
-    ''' Authentication 추가 후에는 토큰에서 각 프로바이더의 인증 정보를 가져와서 사용할 것 '''
+    # TODO: Authentication 추가 후에는 토큰에서 각 프로바이더의 인증 정보를 가져와서 사용할 것
     AWS_ACCESS_KEY_ID = settings.AWS_ACCESS_KEY_ID
     AWS_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
     AWS_DEFAULT_REGION = 'ap-northeast-2'
--- mcp-api-backend/service/instance_service.py
+++ mcp-api-backend/service/instance_service.py
@@ -1,90 +1,46 @@
 import boto3
 import datetime
-from operator import itemgetter
-from config.api_config import settings
 
 
-def get_instance_state():
-    AWS_ACCESS_KEY_ID = settings.AWS_ACCESS_KEY_ID
-    AWS_SECRET_ACCESS_KEY = settings.AWS_SECRET_ACCESS_KEY
-    AWS_DEFAULT_REGION = 'ap-northeast-2'
-
+def get_instance_state(id: str, secret: str, region: str, instanceId: str, metrics: list):
     now = datetime.datetime.utcnow()  # 현재 시각 (UTC 포맷)
     past = now - datetime.timedelta(minutes=60)  # 60분의 timedelta
 
     # AWS Cloudwatch 연결 설정
     client_cw = boto3.client(
         'cloudwatch',
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-        region_name=AWS_DEFAULT_REGION
+        aws_access_key_id=id,
+        aws_secret_access_key=secret,
+        region_name=region
     )
 
-    # AWS EC2 연결 설정
-    client_ec2 = boto3.client(
-        'ec2',
-        aws_access_key_id=AWS_ACCESS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
-        region_name=AWS_DEFAULT_REGION
-    )
+    result = {}
+    for metric in metrics:
+        # instanceId에 대해 metrics를 설정하여 60분간의 평균값을 가져옴
+        stats = client_cw.get_metric_statistics(
+            Namespace='AWS/EC2',
+            MetricName=metric,
+            Dimensions=[{'Name': 'InstanceId', 'Value': instanceId}],
+            StartTime=past,
+            EndTime=now,
+            Period=60,
+            Statistics=['Average']
+        )
 
-    response = client_ec2.describe_instances()  # Amazon EC2의 모든 인스턴스 정보를 가져옴
+        # 통계치 조회 결과
+        datapoints = stats['Datapoints']
 
-    for reservation in response["Reservations"]:
-        print(reservation)
-        for instance in reservation["Instances"]:
+        # last_datapoint = sorted(datapoints, key=itemgetter('Timestamp'))[-1]    # Last result
 
-            # This will print output the value of the Dictionary key 'InstanceId'
-            print(instance["InstanceId"])
+        # # Last utilization
+        # utilization = last_datapoint['Average']
 
-            if (instance["State"]["Name"] == "running"):
-                print("Instance is running")
-            else:
-                print("Instance is not running")
-                continue
+        # # Last utilization timestamp
+        # timestamp = str(last_datapoint['Timestamp'])
 
-            # 각 InstanceID에 대해 CPU 사용률을 가져옴
-            CPUUtilization = client_cw.get_metric_statistics(
-                Namespace='AWS/EC2',
-                MetricName='CPUUtilization',
-                Dimensions=[{'Name': 'InstanceId', 'Value': instance["InstanceId"]}],
-                StartTime=past,
-                EndTime=now,
-                Period=60,
-                Statistics=['Average']
-            )
+        result.update({metric: datapoints})
 
-            # CPU Utilization 결과
-            datapoints = CPUUtilization['Datapoints']
-            print(*datapoints, sep='\n')
-            last_datapoint = sorted(datapoints, key=itemgetter('Timestamp'))[-1]    # Last result
-            # Last utilization
-            utilization = last_datapoint['Average']
-            # Last utilization in %
-            load = round(utilization, 3)
-            # Last utilization timestamp
-            timestamp = str(last_datapoint['Timestamp'])
-            print(f"CPU사용량: {load}% - {timestamp}")
-
-    """
-    StatusCheckFailed_System
-    CPUUtilization
-    StatusCheckFailed_Instance
-    EBSWriteBytes
-    EBSReadOps
-    EBSReadBytes
-    NetworkPacketsIn
-    NetworkIn
-    EBSByteBalance%
-    NetworkOut
-    MetadataNoToken
-    EBSIOBalance%
-    NetworkPacketsOut
-    StatusCheckFailed
-    EBSWriteOps
-    """
-
-    return f"CPU사용량: {load}% - {timestamp}"
+    return result
 
 
 def get_instance_list(id, secret, region):
@@ -101,7 +57,8 @@ def get_instance_list(id, secret, region):
     instance_list = []
     for reservation in response["Reservations"]:
         for instance in reservation["Instances"]:
-            instance_list.append(instance["InstanceId"])
+            if (instance["State"]["Name"] == "running"):
+                instance_list.append(instance["InstanceId"])
 
     return instance_list
 
