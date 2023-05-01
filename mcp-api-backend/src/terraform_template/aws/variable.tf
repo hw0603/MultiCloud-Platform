d8f0ead9ec6ab476aae3d3b546cbf420c639bb45
parkjiyun <pjyun05200@gmail.com>
2023-05-01 18:43:36 +0900

    테라폼 요구 버전 설정 및 프로바이더 관련 변수 설정

---
+++ mcp-api-backend/src/terraform_template/aws/variable.tf
@@ -0,0 +1,15 @@
+# access_key
+variable "aws_access_key" {
+    type = string
+}
+
+# secret_key
+variable "aws_secret_key" {
+    type = string
+}
+
+# region
+variable "aws_region" {
+    type = string
+    default = "ap-northeast-2"
+}
