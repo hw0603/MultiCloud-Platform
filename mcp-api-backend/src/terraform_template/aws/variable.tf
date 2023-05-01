fed194ebb95797022a380cbd26864bc6ee8b8b7e
parkjiyun <pjyun05200@gmail.com>
2023-05-01 18:52:04 +0900

    aws key pair 관련 변수 설정

--- mcp-api-backend/src/terraform_template/aws/variable.tf
+++ mcp-api-backend/src/terraform_template/aws/variable.tf
@@ -13,3 +13,16 @@ variable "aws_region" {
     type = string
     default = "ap-northeast-2"
 }
+
+
+# key_name
+variable "aws_key_name" {
+    type = string
+    default = "web_admin"
+}
+
+# public_key
+variable "aws_public_key_name" {
+    type = string
+    description = "SSH 키를 입력해주세요."
+}
