f03eed86f44ebc230c187aa4cb81cafcdcd4a26c
HyungUk Ryu <hw0603@naver.com>
2023-05-03 18:32:42 +0900

    Chore: fix type

--- mcp-api-backend/db/model/aws_cloudwatch_model.py
+++ mcp-api-backend/db/model/aws_cloudwatch_model.py
@@ -6,7 +6,7 @@ class AwsCloudwatch(Base):
     __tablename__ = "aws_cloudwatch"
     instance_id = Column(String(50), primary_key=True, index=True)
     provider_id = Column(Integer, nullable=False)  # TODO: FK 지정해야 함
-    metrix = Column(String(50), nullable=False)
+    metric = Column(String(50), nullable=False)
     timestamp = Column(DateTime, nullable=False)
     value = Column(Float, nullable=False)
     unit = Column(String(10), nullable=False)
---
+++ mcp-api-backend/repository/aws_cloudwatch_repository.py
@@ -0,0 +1,7 @@
+from sqlalchemy.orm import Session
+import db.model.aws_cloudwatch_model as models
+
+AwsCloudwatchModel = models.AwsCloudwatch
+
+
+raise NotImplementedError
