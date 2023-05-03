ca2e34366fc1855a125dba20fee474c21b9672fb
HyungUk Ryu <hw0603@naver.com>
2023-05-03 21:53:30 +0900

    Fix: Cloudwatch DB 모델이 복합키를 가지도록 수정

--- mcp-api-backend/db/model/aws_cloudwatch_model.py
+++ mcp-api-backend/db/model/aws_cloudwatch_model.py
@@ -1,11 +1,13 @@
 from db.session import Base
-from sqlalchemy import Column, DateTime, Integer, Float, String
+from sqlalchemy import Column, DateTime, Integer, Float, String, PrimaryKeyConstraint, UniqueConstraint
 
 
 class AwsCloudwatch(Base):
     __tablename__ = "aws_cloudwatch"
-    instance_id = Column(String(50), primary_key=True, index=True)
-    provider_id = Column(Integer, nullable=False)  # TODO: FK 지정해야 함
+    __table_args__ = (PrimaryKeyConstraint('instance_id', 'provider_id'), )  # 복합키
+
+    instance_id = Column(String(50), index=True, nullable=False)
+    provider_id = Column(Integer, index=True, nullable=False)  # TODO: FK 지정해야 함
     metric = Column(String(50), nullable=False)
     timestamp = Column(DateTime, nullable=False)
     value = Column(Float, nullable=False)
