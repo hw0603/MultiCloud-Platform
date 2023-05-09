a4463defb02f76917145e6b9d47bce8b1fc875fe
lee-ji-an <leeja0407@naver.com>
2023-05-09 23:29:43 +0900

    Feat: vpc 생성 로직 분리

---
+++ mcp-api-backend/src/terraform_template/aws/vpc/vpc_variable.tf
@@ -0,0 +1,28 @@
+# VPC
+variable "aws_vpc_cidr" {
+    type = string
+    description = "허용된 CIDR 블록 사이즈는 입니다."
+    default = "10.0.0.0/16"
+}
+
+variable "aws_vpc_name" {
+    type = string
+    description = "VPC 이름을 특수문자 없이 입력하세요."
+    default = "aws vpc name"
+}
+
+# Internet Gateway
+variable "aws_internet_gateway_name" {
+    type = string
+    description = "INTERNET GATEWAY 이름을 특수문자 없이 입력하세요."
+    default = "aws igw name"
+}
+
+
+# Provider Setting
+
+# region
+variable "aws_region" {
+    type = string
+    default = "ap-northeast-2"
+}
