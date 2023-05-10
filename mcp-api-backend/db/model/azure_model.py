2ce4d8301d77bb182ae38ecbf5872d9aaefe4267
parkjiyun <pjyun05200@gmail.com>
2023-05-10 20:19:31 +0900

    Establish: azure model 추가

---
+++ mcp-api-backend/db/model/azure_model.py
@@ -0,0 +1,17 @@
+import datetime
+
+from db.session import Base 
+from sqlalchemy import Column, DateTime, Integer, String, UniqueConstraint
+
+
+class Azure_provider(Base):
+    __tablename__ = "azure_provider"
+    id = Column(Integer, primary_key=True, index=True)
+    environment = Column(String(200), nullable=False)
+    team = Column(String(200), nullable=False)
+    client_id = Column(String(200), nullable=False)
+    client_secret = Column(String(200), nullable=False)
+    subscription_id = Column(String(200), nullable=False)
+    tenant_id = Column(String(200), nullable=False)
+    created_at = Column(DateTime, default=datetime.datetime.now())
+    __table_args__ = (UniqueConstraint("team", "environment"),)
