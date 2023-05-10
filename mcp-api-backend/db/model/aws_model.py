2e6d6b5048f021bf8c867408afea8f4f51121e83
parkjiyun <pjyun05200@gmail.com>
2023-05-10 19:27:43 +0900

    Establish: aws model 추가

---
+++ mcp-api-backend/db/model/aws_model.py
@@ -0,0 +1,19 @@
+import datetime
+
+from config.database_config import Base
+from sqlalchemy import Column, DateTime, Integer, String, UniqueConstraint
+
+
+class Aws_provider(Base):
+    __tablename__ = "aws_provider"
+    id = Column(Integer, primary_key=True, index=True)
+    environment = Column(String(200), nullable=False)
+    team = Column(String(200), nullable=False)
+    access_key_id = Column(String(200), nullable=False)
+    secret_access_key = Column(String(200), nullable=False)
+    default_region = Column(String(200))
+    profile_name = Column(String(200), nullable=False)
+    role_arn = Column(String(200), nullable=True)
+    source_profile = Column(String(200), nullable=True)
+    created_at = Column(DateTime, default=datetime.datetime.now())
+    __table_args__ = (UniqueConstraint("team", "environment"),)
