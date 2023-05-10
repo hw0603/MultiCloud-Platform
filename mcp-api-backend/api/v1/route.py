980524e05ad3e99f922f0271b98cd76b242936a6
HyungUk Ryu <hw0603@naver.com>
2023-05-10 16:34:45 +0900

    Chore: 테스트 용 API 추가

--- mcp-api-backend/api/v1/route.py
+++ mcp-api-backend/api/v1/route.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from . import hello, adminInfo, instance
+from . import hello, adminInfo, instance, testapi
 
 
 api_router = APIRouter()
+api_router.include_router(testapi.router, prefix="/test", tags=["Test"])
 api_router.include_router(hello.router, prefix="/hello", tags=["Hello"])
 api_router.include_router(adminInfo.router, prefix="/adminInfo", tags=["AdminInfo"])
 api_router.include_router(instance.router, prefix="/instance", tags=["Instance"])
