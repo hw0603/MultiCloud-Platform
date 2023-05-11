7ed605571681934c74a67d87d4daf4d1ebc40e5e
HyungUk Ryu <hw0603@naver.com>
2023-05-12 00:49:17 +0900

    Feat: Variable API 비즈니스 로직 추가

--- mcp-api-backend/api/v1/variable.py
+++ mcp-api-backend/api/v1/variable.py
@@ -1,25 +1,25 @@
 from fastapi import APIRouter, Depends
-
+from service import variable_service
 
 router = APIRouter()
 
 
 @router.get("/json")
 async def get_json(
-    stack_json: dict# = Depends(...),
+    stack_json: dict = Depends(variable_service.get_json),
 ):
     return stack_json
 
 
 @router.get("/list")
 async def get_list(
-    stack_list: list# = Depends(...),
+    stack_list: list = Depends(variable_service.get_list),
 ):
     return stack_list
 
 
 # @router.get("/deploy/{deploy_id}")
 # async def get_deploy_by_id(
-#     deploy_variables: dict = Depends(...),
+#     deploy_variables: dict = Depends(get.get_deploy_by_id),
 # ):
 #     return deploy_variables
