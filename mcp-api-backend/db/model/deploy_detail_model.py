e507851dad69440d2e507c695b2cf54570cb9cec
parkjiyun <pjyun05200@gmail.com>
2023-05-11 14:21:18 +0900

    Feat: 해당 테이블에 foreign key 추가

--- mcp-api-backend/db/model/deploy_detail_model.py
+++ mcp-api-backend/db/model/deploy_detail_model.py
@@ -1,11 +1,13 @@
 import datetime
 
 from db.session import Base
-from sqlalchemy import JSON, Column, DateTime, Integer, String
+from sqlalchemy import JSON, Column, DateTime, Integer, String, ForeignKey
 
 
 class DeployDetail(Base):
     __tablename__ = "deploy_detail"
     id = Column(Integer, primary_key=True, index=True)
+    deploy_id = Column(Integer, ForeignKey("deploy.deploy_id"))
+    stack_id = Column(Integer, ForeignKey("stack.stack_id"))
     tfvar_file = Column(String(50), nullable=True)
     variables = Column(JSON)
