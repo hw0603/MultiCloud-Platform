5524af721fb2eff7df66b7a4d858a99b97e946a8
HyungUk Ryu <hw0603@naver.com>
2023-05-13 17:26:07 +0900

    Remove: 임시로 추가했던 /hello API 삭제

--- mcp-api-backend/api/v1/route.py
+++ mcp-api-backend/api/v1/route.py
@@ -9,7 +9,6 @@ async def status_check():
     return {"status": "Connected"}
 
 api_router.include_router(testapi.router, prefix="/test", tags=["Test"])
-api_router.include_router(hello.router, prefix="/hello", tags=["Hello"])
 api_router.include_router(adminInfo.router, prefix="/adminInfo", tags=["AdminInfo"])
 api_router.include_router(instance.router, prefix="/instance", tags=["Instance"])
 api_router.include_router(stack.router, prefix="/stacks", tags=["Stack"])
