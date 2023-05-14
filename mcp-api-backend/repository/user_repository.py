a8a83632fa3ed45dfbf7ba390a62964d400debd7
lee-ji-an <leeja0407@naver.com>
2023-05-14 21:09:13 +0900

    Feat: db에 초기 생성자 생성 로직 추가

--- mcp-api-backend/repository/user_repository.py
+++ mcp-api-backend/repository/user_repository.py
@@ -1,10 +1,32 @@
 from sqlalchemy.orm import Session
 import db.model.user_model as models
+from config.user_config import settings
 
 Usermodel = models.User
+
 
 def get_user_by_username(db: Session, username: str):
     try:
         return db.query(Usermodel).filter(Usermodel.username == username).first()
+    except Exception as err:
+        raise err
+
+
+def create_init_user(db: Session, password: str):
+    db_user = models.User(
+        username=settings.INIT_USER.get("username"),
+        password=password,  # TODO: password hashing 하는 것으로 바꿔야 함
+        fullname=settings.INIT_USER.get("fullname"),
+        email=settings.INIT_USER.get("email"),
+        role="{System_Manager : True}",
+        team="team",
+        is_active=True
+    )
+    try:
+        db.add(db_user)
+        print(db_user.team)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
     except Exception as err:
         raise err
