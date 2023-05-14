fc9f6cf6bcb4ae709d38286f0b3490d1e66fb4a1
lee-ji-an <leeja0407@naver.com>
2023-05-14 21:34:28 +0900

    Feat: 초기 사용자 생성 api router 추가

--- mcp-api-backend/api/v1/route.py
+++ mcp-api-backend/api/v1/route.py
@@ -1,8 +1,9 @@
 from fastapi import APIRouter
-from . import hello, adminInfo, instance
+from . import hello, adminInfo, instance, user
 
 
 api_router = APIRouter()
 api_router.include_router(hello.router, prefix="/hello", tags=["Hello"])
 api_router.include_router(adminInfo.router, prefix="/adminInfo", tags=["AdminInfo"])
 api_router.include_router(instance.router, prefix="/instance", tags=["Instance"])
+api_router.include_router(user.router, prefix="/user", tags=["user"])
