aceb67e7c6c2183a33948115c4f6524b88634056
parkjiyun <pjyun05200@gmail.com>
2023-05-15 15:56:21 +0900

    Establish: aws Provider repository에 aws profile 생성 함수 추가

---
+++ mcp-api-backend/repository/aws_repository.py
@@ -0,0 +1,35 @@
+import datetime
+
+from sqlalchemy.orm import Session
+import db.model.aws_model as models
+import entity.aws_entity as schemas_aws
+
+
+def create_aws_profile(db: Session, aws: schemas_aws.AwsAsumeProfile):
+    db_aws = models.Aws_provider(
+        access_key_id=aws.access_key_id,
+        secret_access_key=aws.secret_access_key,
+        default_region=aws.default_region,
+        environment=aws.environment,
+        profile_name=aws.profile_name,
+        role_arn=aws.role_arn,
+        source_profile=aws.source_profile,
+        created_at=datetime.datetime.now(),
+        team=aws.team,
+
+    )
+    check_None = [None, "string"]
+    if db_aws.role_arn in check_None:
+        db_aws.role_arn = ""
+    if db_aws.profile_name in check_None:
+        db_aws.profile_name = ""
+    if db_aws.source_profile in check_None:
+        db_aws.source_profile = ""
+    try:
+        db.add(db_aws)
+        db.commit()     # 모든 작업의 정상 처리를 확정하는 명령어, 버퍼에 올라왔던 사항들을 모두 DB에 반영
+        db.refresh(db_aws) # DB에서 최신 상태의 객체를 불러오는 명령어
+        return db_aws
+    except Exception as err:
+        raise err
+    
