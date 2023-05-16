b8f3406d9f72cd53fd1f57d5f52da3da60071709
HyungUk Ryu <hw0603@naver.com>
2023-05-16 16:31:27 +0900

    Refactor: 현재 경로의 모든 모델을 동적으로 import 하도록 변경

--- mcp-api-backend/db/model/__init__.py
+++ mcp-api-backend/db/model/__init__.py
@@ -1,29 +1,18 @@
-from db.model import (
-    activity_log_model,
-    aws_cloudwatch_model,
-    aws_model,
-    azure_model,
-    custom_provider_model,
-    deploy_detail_model,
-    deploy_model,
-    gcp_model,
-    stack_model,
-    task_model,
-    team_model,
-    user_model
-)
-
+from os.path import dirname, basename, isfile, join
+import glob
+modules = glob.glob(join(dirname(__file__), "*.py"))
 __all__ = [
-    "activity_log_model",
-    "aws_cloudwatch_model",
-    "aws_model",
-    "azure_model",
-    "custom_provider_model",
-    "deploy_detail_model",
-    "deploy_model",
-    "gcp_model",
-    "stack_model",
-    "task_model",
-    "team_model",
-    "user_model",
+    basename(f)[:-3] for f in modules if isfile(f) and not f.endswith('__init__.py')
 ]
+from db.model import *
+
+
+# 모든 모델을 import
+# 참고) https://stackoverflow.com/questions/9088957/sqlalchemy-cannot-find-a-class-name
+"""
+From the SQLAlchemy documentation:
+However, due to the behavior of SQLAlchemy's "declarative" configuration mode,
+all modules which hold active SQLAlchemy models need to be imported before those models can successfully be used.
+So, if you use model classes with a declarative base,
+you need to figure out a way to get all your model modules imported to be able to use them in your application.
+"""
