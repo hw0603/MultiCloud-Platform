1bee9ca8324d58ef0d1a45754aa8c74801b347d0
lee-ji-an <leeja0407@naver.com>
2023-05-17 05:02:34 +0900

    Feat: user 생성 로직 추가

--- mcp-api-backend/repository/user_repository.py
+++ mcp-api-backend/repository/user_repository.py
@@ -1,7 +1,9 @@
 from sqlalchemy.orm import Session
 import db.model.user_model as models
 from config.api_config import settings
+from sqlalchemy import exc
 import entity.user_entity as schemas
+from src.shared.security.vault import get_password_hash
 
 Usermodel = models.User
 
@@ -9,6 +11,19 @@ Usermodel = models.User
 def get_user_by_username(db: Session, username: str):
     try:
         return db.query(Usermodel).filter(Usermodel.username == username).first()
+    except Exception as err:
+        raise err
+
+def create_user(db: Session, user: schemas.UserCreate):
+    db_user = Usermodel(**user.dict())
+    db_user.password = get_password_hash(user.password)
+    try:
+        db.add(db_user)
+        db.commit()
+        db.refresh(db_user)
+        return db_user
+    except exc.IntegrityError as err:
+        raise ValueError(str(err.__dict__["orig"]))
     except Exception as err:
         raise err
 
--- mcp-api-backend/service/user/user_service.py
+++ mcp-api-backend/service/user/user_service.py
@@ -1,4 +1,7 @@
+from multiprocessing import connection
 import os
+from re import U
+from unittest import result
 from fastapi import HTTPException, Depends
 
 from config.api_config import settings
@@ -6,8 +9,9 @@ from repository import user_repository as crud_users
 from sqlalchemy.orm import Session
 from db.connection import get_db
 from utils.utils import object_as_dict
-from entity.user_entity import UserInit
+from entity.user_entity import UserInit, UserCreate, User
 from service.user import user_util
+from src.shared.security import deps
 
 def get_admin_info(db: Session):
     user = crud_users.get_user_by_username(db, "admin")
@@ -25,3 +29,20 @@ async def create_init_user(passwd: UserInit, db: Session = Depends(get_db)):
             return crud_users.create_init_user(db=db, password=passwd.password)
         except Exception as err:
             raise HTTPException(status_code=400, detail=str(err))
+
+async def create_user(
+        user: UserCreate, 
+        current_user: User = Depends(deps.get_current_active_user),
+        db: Session = Depends(get_db)
+):
+    # TODO: user 권한 validation
+    db_user = crud_users.get_user_by_username(db, username=user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Username already registered")
+    user_util.validate_password(user.username, user.password)
+    try:
+        result = crud_users.create_user(db=db, user=user)
+        # TODO: logging 추가
+        return result
+    except Exception as err:
+        raise HTTPException(status_code=400, detail=str(err))
