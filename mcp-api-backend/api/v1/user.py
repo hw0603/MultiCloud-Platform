3b81ee3992fb83ec01465858d80e7c62801e4ea3
lee-ji-an <leeja0407@naver.com>
2023-05-17 10:56:05 +0900

    Feat: user 생성 라우터 추가

--- mcp-api-backend/api/v1/user.py
+++ mcp-api-backend/api/v1/user.py
@@ -6,6 +6,7 @@ from service.user import user_service
 from entity.user_entity import (
     User,
     UserInit,
+    UserCreate
 )
 
 router = APIRouter()
@@ -15,3 +16,7 @@ async def create_init_user(
     create_init_user: UserInit = Depends(user_service.create_init_user),
 ):
     return create_init_user
+
+@router.post("/", response_model=User)
+async def create_user(create_user: UserCreate = Depends(user_service.create_user)):
+    return create_user
