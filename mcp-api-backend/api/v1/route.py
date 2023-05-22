08a0b0950b783c8aec9cfc5666bf35fa1a61103c
parkjiyun <pjyun05200@gmail.com>
2023-05-22 16:24:13 +0900

    Establish: gcp create, get, delete 기능 추가

--- mcp-api-backend/api/v1/route.py
+++ mcp-api-backend/api/v1/route.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from api.v1 import adminInfo, instance, stack, variable, activity_log, user, aws
+from api.v1 import adminInfo, instance, stack, variable, activity_log, user, aws, gcp
 
 
 api_router = APIRouter()
@@ -15,3 +15,4 @@ api_router.include_router(stack.router, prefix="/stacks", tags=["Stack"])
 api_router.include_router(variable.router, prefix="/variable", tags=["Variable"])
 api_router.include_router(activity_log.router, prefix="/activity_log", tags=["ActivityLog"])
 api_router.include_router(aws.router, prefix="/aws", tags=["Aws"])
+api_router.include_router(gcp.router, prefix="/gcp", tags=["Gcp"])
