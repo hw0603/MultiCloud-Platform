141e94fc5e08d718f7b62d29921fcdd60b7f4d3e
dltmdrl1244 <dltmdrl1244@naver.com>
2023-06-01 02:10:47 +0900

    Feat: 활동 로그 조회 화면 구현

--- mcp-dashboard/src/pages/Log.jsx
+++ mcp-dashboard/src/pages/Log.jsx
@@ -1,9 +1,73 @@
-import React from "react";
+import React, { useLayoutEffect, useMemo, useState, useEffect } from "react";
+import { useStateContext } from "../contexts/ContextProvider";
+import axios from "axios";
+import { Table } from "../components";
+// import "../components/Modal.css";
+// import { MdOutlineCancel } from "react-icons/md";
+// import { BsFillTrashFill } from "react-icons/bs"
 
 const Log = () => {
+    const { mainColor, base_url, stacks, setStacks, isAuthorized, isModalOpen, setIsModalOpen } = useStateContext();
+    const [logs, setLogs] = useState([]);
+
+    const getLog = () => {
+        axios({
+            method: 'GET',
+            url: `${base_url}/api/v1/activity_log/id/admin`,
+            headers: {
+                "Authorization": localStorage.getItem("accessToken")
+            },
+        })
+            .then((response) => {
+                setLogs(response.data);
+            })
+            .catch((error) => {
+                console.log("error", error);
+            })
+    }
+
+    const columns = useMemo(
+        () => [
+            {
+                accessor: "id",
+                Header: "ID",
+            },
+            {
+                accessor: "team",
+                Header: "팀명"
+            },
+            {
+                accessor: "username",
+                Header: "사용자명"
+            },
+            {
+                accessor: "action",
+                Header: "내용"
+            },
+            {
+                accessor: "created_at",
+                Header: "시간"
+            },
+        ], []
+    );
+
+    useLayoutEffect(() => {
+        getLog();
+    }, []);
+
     return (
         <>
-        <h1>활동 로그 화면</h1>
+            <div className="px-10 py-5">
+                <div className="flex justify-between p-3 m-3 border-b-2">
+                    <div className="flex justfiy-center items-center text-2xl">활동 로그 </div>
+                </div>
+
+                
+                <Table columns={columns} data={logs} />
+
+            </div>
+
+            {/* {isModalOpen && <Modal />} */}
         </>
     );
 };
