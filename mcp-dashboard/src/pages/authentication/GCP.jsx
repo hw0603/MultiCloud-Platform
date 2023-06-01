42f97e2253e65cc388e744b8939f879c45a948a1
dltmdrl1244 <dltmdrl1244@naver.com>
2023-06-01 22:44:42 +0900

    Refactor: Modal 창 모듈화에 따른 코드 수정 모든 코드에 중복해서 정의되어있던 const Modal을 모듈로서 import해서 사용함에 따라 ModalComponent만 정의하도록 코드 수정

--- mcp-dashboard/src/pages/authentication/GCP.jsx
+++ mcp-dashboard/src/pages/authentication/GCP.jsx
@@ -2,7 +2,7 @@ import React, { useLayoutEffect, useMemo, useState, useEffect } from "react";
 import { useStateContext } from "../../contexts/ContextProvider";
 import axios from "axios";
 import { redirect, useNavigate } from "react-router-dom";
-import { Table, Button } from "../../components";
+import { Table, Button, Modal } from "../../components";
 import "../../components/Modal.css";
 import { MdOutlineCancel } from "react-icons/md";
 import { BsFillTrashFill } from "react-icons/bs"
@@ -11,12 +11,8 @@ const refresh = () => {
     window.location.replace("/GCP");
 }
 
-const Modal = () => {
+const ModalComponentCreateProvider = () => {
     const { setIsModalOpen, mainColor, base_url } = useStateContext();
-    const closeModal = () => {
-        setIsModalOpen(false);
-    }
-
     const createProvider = () => {
         const provider_team = document.getElementById("provider_team").value;
         const provider_environment = document.getElementById("provider_environment").value;
@@ -33,12 +29,11 @@ const Modal = () => {
                 data: {
                     team: provider_team,
                     environment: provider_environment,
-                    gcloud_keyfile_json: {provider_GKJson},
+                    gcloud_keyfile_json: { provider_GKJson },
                 }
             })
                 .then((response) => {
                     console.log(response);
-                    closeModal();
                     alert("프로바이더가 생성되었습니다.");
                     refresh();
                 })
@@ -49,57 +44,40 @@ const Modal = () => {
     }
 
     return (
-        <div className="absolute left-0 top-0 min-w-full min-h-full bg-black/50 flex justify-center items-center" id="modal_mask" onClick={closeModal}>
-            <div className="m-10 bg-white w-1/3 p-10 rounded-2xl" onClick={(event) => {
-                event.stopPropagation();
-            }
-            }>
-                <div className="flex justify-between items-center">
-                    <h1 className="font-bold text-xl">프로바이더 생성</h1>
-                    <Button
-                        icon={<MdOutlineCancel />}
-                        color="rgb(153, 171, 180)"
-                        bgHoverColor="light-gray"
-                        size="2xl"
-                        borderRadius="50%"
-                        onClickFunc={closeModal}
-                    />
+        <>
+            <div className="mt-6 flex flex-col gap-4">
+                <div>
+                    <p className="mb-1">Team</p>
+                    <input id="provider_team" type="text" className="border-2 p-2 rounded-xl w-full" />
+                </div>
+                <div>
+                    <p className="mb-1">Environment</p>
+                    <input id="provider_environment" type="text" className="border-2 p-2 rounded-xl w-full" />
+                </div>
+                <div>
+                    <p className="mb-1">GCloud Keyfile JSON</p>
+                    <input id="provider_GKJson" type="text" className="border-2 p-2 rounded-xl w-full" />
                 </div>
 
-                <div className="mt-6 flex flex-col gap-4">
-                    <div>
-                        <p className="mb-1">Team</p>
-                        <input id="provider_team" type="text" className="border-2 p-2 rounded-xl w-full" />
-                    </div>
-                    <div>
-                        <p className="mb-1">Environment</p>
-                        <input id="provider_environment" type="text" className="border-2 p-2 rounded-xl w-full" />
-                    </div>
-                    <div>
-                        <p className="mb-1">GCloud Keyfile JSON</p>
-                        <input id="provider_GKJson" type="text" className="border-2 p-2 rounded-xl w-full" />
-                    </div>
-                    
-                    
-                </div>
-                <div className="mt-10">
-                    <Button
-                        color="white"
-                        bgColor={mainColor}
-                        text="새 프로바이더 생성"
-                        borderRadius="10px"
-                        onClickFunc={() => {
-                            if (window.confirm("프로바이더를 생성하시겠습니까?"))
-                            {
-                                createProvider();
-                            }
-                        }}
-                    />
-                </div>
+
             </div>
-        </div>
+            <div className="mt-10">
+                <Button
+                    color="white"
+                    bgColor={mainColor}
+                    text="새 프로바이더 생성"
+                    borderRadius="10px"
+                    onClickFunc={() => {
+                        if (window.confirm("프로바이더를 생성하시겠습니까?")) {
+                            createProvider();
+                        }
+                    }}
+                />
+            </div></>
     );
+
 }
+
 
 const GCP = () => {
     const { mainColor, base_url, stacks, setStacks, isAuthorized, isModalOpen, setIsModalOpen } = useStateContext();
@@ -204,7 +182,7 @@ const GCP = () => {
                 <Table columns={columns} data={providers} />
             </div>
 
-            {isModalOpen && <Modal />}
+            {isModalOpen && <Modal title={"새 프로바이더 생성"}><ModalComponentCreateProvider /></Modal>}
         </>
     );
 };
