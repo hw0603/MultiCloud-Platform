8cd50eb61ebaa5e5e3a2abdd471b77906c4c8517
dltmdrl1244 <dltmdrl1244@naver.com>
2023-06-05 01:12:45 +0900

    Feat: 작업 현황 페이지 구현

--- mcp-dashboard/src/pages/OpLog.jsx
+++ mcp-dashboard/src/pages/OpLog.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const OpLog = () => {
     return (
-        <>
-        <h1>작업 로그 화면</h1>
-        </>
+        <div className="flex w-full h-full">
+            <iframe src="http://localhost:3001/d/Airflow_Monitor/airflow-monitoring?orgId=1&from=1685715567969&to=1685888367970&theme=light&refresh=5s&kiosk" frameborder="0" className="w-full h-full"></iframe>
+        </div>
     );
 };
 
